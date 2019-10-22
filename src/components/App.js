acd766f72b47a0d1b206eeb940ce9b6605cee57c
阿部直也 <n.abe@gemcook.com>
2019-10-22 11:33:49 +0900

    image stateの追加。 axiosを叩く際にasync await, try catchの追加

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,29 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit = term => {
+  state = {images: []};
+
+  onSearchSubmit = async term => {
     const API_KEY = process.env.REACT_APP_PIXABAY_APIKEY;
-    axios.get(`https://pixabay.com/api/?key=${API_KEY}`, {
-      params: {q: term},
-    });
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=${API_KEY}`,
+        {
+          params: {q: term},
+        }
+      );
+      console.log(response.data.hits);
+      this.setState({images: response.data.hits});
+    } catch {
+      window.alert('写真の取得に失敗しました。');
+    }
   };
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
