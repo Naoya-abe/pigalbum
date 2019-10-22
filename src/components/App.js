62e3d1d3b5e5367360918957dc36598811385425
阿部直也 <n.abe@gemcook.com>
2019-10-22 11:18:21 +0900

    axiosの導入、検索バーでaxios.getできた

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit = term => {
-    console.log(term);
+    const API_KEY = process.env.REACT_APP_PIXABAY_APIKEY;
+    axios.get(`https://pixabay.com/api/?key=${API_KEY}`, {
+      params: {q: term},
+    });
   };
 
   render() {
